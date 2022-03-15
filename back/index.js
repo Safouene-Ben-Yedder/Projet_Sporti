52b6b48d71f4c91d409ffc92edabb7adb88cbf1a
syrinejendoubi <syrine.jendoubi@esen.tn>
2022-03-15 15:37:34 +0100

    connexion à la base

--- back/index.js
+++ back/index.js
@@ -1,9 +1,26 @@
-const express = require('express')
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
 
-const PORT = process.env.PORT || 3001
+dotenv.config();
 
-const app = express()
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(console.log("Connected to Mongo"))
+	.catch((err) => console.log(err));
+var corsOptions = {
+	origin: "http://localhost:3000",
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
-	console.log(`Server listening on ${PORT}`)
-})
+	console.log(`Server listening on ${PORT}`);
+});
