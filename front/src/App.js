57363fdbcfbb707a467bffc401a3b979d4b6aa90
omarkamoun06 <omar.kamoun@istic.u-carthage.tn>
2022-04-06 12:09:37 +0100

    Invitation

--- front/src/App.js
+++ front/src/App.js
@@ -9,6 +9,9 @@ import { Paiementform } from "./components/paiementform/Paiementform";
 import LieuForm from "./components/lieuForm/LieuForm";
 
 import RegisterPage from "./pages/RegisterCoach/RegisterPage";
+import InvitationPage from "./pages/invitationpage/InvitationPage";
+import InvitationForm from "./components/invitationform/invitationform";
+import PasswordForm from "./components/invitationform/passwordform";
 function App() {
 	return (
 		<div>
@@ -33,11 +36,14 @@ function App() {
 
 					<Route exact path="/lieu-page">
 						<LieuPage />
-
 					</Route>
 					<Route exact path="/register-page">
 						<RegisterPage />
 					</Route>
+					<Route path="/invitation">
+						<InvitationPage />
+					</Route>
+					<Route path="/addPass" component={PasswordForm} />
 				</Switch>
 			</Router>
 		</div>
